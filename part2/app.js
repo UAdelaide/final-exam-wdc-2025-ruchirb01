c36cc77c0ca29707846701a48d851e37d0b31eed
ruchirb01 <ruchir.bhatnagar@student.adelaide.edu.au>
2025-06-20 15:10:08 +0930

    Update app.js

--- part2/app.js
+++ part2/app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
-// Middleware
+// Session configuration
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'noonobnenbeonboenoenroerkoka',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, 
+    maxAge: 24 * 60 * 60 * 1000 // 24 h
+  }
+}));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-// Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// Export the app instead of listening here
 module.exports = app;
